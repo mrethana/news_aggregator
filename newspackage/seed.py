c191ea34aa618eed5c14559ac54ebff633ef6c4a
Mark Rethana <mark.rethana@gmail.com>
2018-11-14 16:39:38 -0500

    first youtube test pass

--- newspackage/seed.py
+++ newspackage/seed.py
@@ -109,13 +109,15 @@ def add_content_objects():
         print(content)
         session.commit()
 
-print('Diet search...')
-diet = intermediate_search()
-find_or_create_content(diet)
+# print('Diet search...')
+# diet = intermediate_search()
+# find_or_create_content(diet)
+#
+# print('Tweets search...')
+# tweets = twitter_api_call(twitter_handles)
+# find_or_create_content(tweets)
 
-print('Tweets search...')
-tweets = twitter_api_call(twitter_handles)
-find_or_create_content(tweets)
+
 
 add_medium_objects()
 add_provider_objects()
--- newspackage/youtube_data.py
+++ newspackage/youtube_data.py
@@ -3,6 +3,9 @@ from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 import pandas as pd
 import pprint
+import pafy
+import warnings
+warnings.filterwarnings("ignore")
 from apikeys import *
 
 
@@ -10,7 +13,7 @@ DEVELOPER_KEY = youtube_key
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-def youtube_search(q, max_results=50,order="relevance", token=None, location=None, location_radius=None):
+def youtube_search(q, max_results=50,order="date", token=None, location=None, location_radius=None):
 
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
 
@@ -42,8 +45,12 @@ def youtube_search(q, max_results=50,order="relevance", token=None, location=Non
             categoryId = (response['items'][0]['snippet']['categoryId'])
             favoriteCount = (response['items'][0]['statistics']['favoriteCount'])
             viewCount = (response['items'][0]['statistics']['viewCount'])
-            likeCount = (response['items'][0]['statistics']['likeCount'])
-            dislikeCount = (response['items'][0]['statistics']['dislikeCount'])
+#             likeCount = (response['items'][0]['statistics']['likeCount'])
+#             dislikeCount = (response['items'][0]['statistics']['dislikeCount'])
+            date = pd.to_datetime((response['items'][0]['snippet']['publishedAt'])).date().strftime('%Y-%m-%d')
+            description = response['items'][0]['snippet']['localized']['description']
+            url = 'https://www.youtube.com/watch?v='+videoId
+            image_url = response['items'][0]['snippet']['thumbnails']['default']['url']
 
         if 'commentCount' in response['items'][0]['statistics'].keys():
             commentCount = (response['items'][0]['statistics']['commentCount'])
@@ -55,6 +62,34 @@ def youtube_search(q, max_results=50,order="relevance", token=None, location=Non
         else:
             tags = []
 
-        youtube_dict = {'tags':tags,'channelId': channelId,'channelTitle': channelTitle,'categoryId':categoryId,'title':title,'videoId':videoId,'viewCount':viewCount,'likeCount':likeCount,'dislikeCount':dislikeCount,'commentCount':commentCount,'favoriteCount':favoriteCount}
+        youtube_dict = {'tags':tags,'source_id': channelId,'source': channelTitle,'categoryId':categoryId,'title':title,'videoId':videoId,'viewCount':viewCount,'commentCount':commentCount,'favoriteCount':favoriteCount,
+                        'formality':'Intermediate', 'medium':'video','date':date, 'description': description, 'web_url':url, 'image_url':image_url}
         all_dicts.append(youtube_dict)
     return pd.DataFrame(all_dicts)
+
+def add_length(df):
+    all_lengths = []
+    for index, row in df.iterrows():
+        try:
+            length = round(pafy.new(row.web_url).length / 60)
+        except:
+            length = 2
+        all_lengths.append(length)
+    df['length'] = all_lengths
+    return df
+
+def add_category(df):
+    cats = ['keto','ketogenic','paleo','paleolithic','vegan','vegetarian']
+    all_params = []
+    for index, row in df.iterrows():
+        try:
+            intersect = list(set(row.tags).intersection(cats))
+            if len(intersect) > 0:
+                category = intersect[0]
+            else:
+                category = row.tags[0]
+        except:
+            category = 'none'
+        all_params.append(category)
+    df['param'] = all_params
+    return df
