3de9e3e0bb9e706b60fc88d5d0988cb39e6e3334
Mark Rethana <mark.rethana@gmail.com>
2019-01-08 20:32:45 -0500

    fb dummy data

--- newspackage/social.py
+++ newspackage/social.py
@@ -62,14 +62,22 @@ def twitter_api_call(list_handles, categories):
     return empty
 
 #FACEBOOK
-nyt_fb_post = ['https://www.facebook.com/WellNYT/posts/2030812243705795', 'https://www.facebook.com/WellNYT/posts/2027922970661389',
-              'https://www.facebook.com/WellNYT/posts/2027920193995000','https://www.facebook.com/WellNYT/posts/2023650137755339',
-              'https://www.facebook.com/WellNYT/posts/2022428551210831','https://www.facebook.com/WellNYT/posts/2022404617879891']
+nyt_fb_post = [('https://www.facebook.com/WellNYT/posts/2030812243705795','NYT Health','text'),
+('https://www.facebook.com/WellNYT/posts/2027922970661389','NYT Health','text'),
+              ('https://www.facebook.com/WellNYT/posts/2027920193995000','NYT Health','text'),
+              ('https://www.facebook.com/WellNYT/posts/2023650137755339','NYT Health','text'),
+              ('https://www.facebook.com/WellNYT/posts/2022428551210831','NYT Health','text'),
+              ('https://www.facebook.com/WellNYT/posts/2022404617879891','NYT Health','text')]
 
-nyt_fb_vid = ['https://www.facebook.com/WellNYT/videos/vl.215269862225520/1148312565289105/?type=1','https://www.facebook.com/WellNYT/videos/vl.215269862225520/1146634572123571/?type=1',
-         'https://www.facebook.com/WellNYT/videos/vl.215269862225520/1146398342147194/?type=1', 'https://www.facebook.com/WellNYT/videos/1437656589688033/'
-         ,'https://www.facebook.com/WellNYT/videos/1408624012591291/','https://www.facebook.com/WellNYT/videos/1390021831118176/','https://www.facebook.com/WellNYT/videos/1363150167138676/',
-         'https://www.facebook.com/WellNYT/videos/1362877763832583/']
+nyt_fb_vid = [('https://www.facebook.com/WellNYT/videos/vl.215269862225520/1148312565289105/?type=1','NYT Health','video')
+,('https://www.facebook.com/WellNYT/videos/vl.215269862225520/1146634572123571/?type=1', 'NYT Health','video'),
+         ('https://www.facebook.com/WellNYT/videos/vl.215269862225520/1146398342147194/?type=1','NYT Health','video')
+         , ('https://www.facebook.com/WellNYT/videos/1437656589688033/','NYT Health','video')
+         ,('https://www.facebook.com/WellNYT/videos/1408624012591291/', 'NYT Health','video'),
+         ('https://www.facebook.com/WellNYT/videos/1390021831118176/','NYT Health','video'),
+         ('https://www.facebook.com/WellNYT/videos/1363150167138676/','NYT Health','video'),
+         ('https://www.facebook.com/WellNYT/videos/1362877763832583/','NYT Health','video'),
+         ('https://www.facebook.com/WellNYT/videos/1362877763832583/','NYT Health','video')]
 
 def random_cat(list_categories, length):
     cats = []
@@ -85,16 +93,14 @@ def random_length(length):
 
 
 
-def create_sample_fb(list_urls, categories_list, medium_type):
+def create_sample_fb(list_tuples, categories_list):
     diets = ['vegan','keto','vegetarian','paleo']
     df = pd.DataFrame()
-    df['web_url'] = list_urls
-    df['image_url'] = list_urls
     df['description'] = 'Facebook Dummy Data'
     df['title'] = 'Facebook Dummy Data'
     df['date'] = '2018-12-26'
-    df['medium'] = medium_type
-    df['source'] = 'NYT Health'
+    # df['medium'] = medium_type
+    # df['source'] = 'NYT Health'
     df['source_id'] = 'Facebook'
     df['formality'] = 'Informal'
     length = len(df.web_url)
@@ -105,26 +111,47 @@ def create_sample_fb(list_urls, categories_list, medium_type):
     df['param_3'] = random_cat(categories_list,length)
     df['param_4'] = random_cat(categories_list,length)
     df['param_5'] = random_cat(categories_list,length)
+    urls = []
+    images = []
+    sources = []
+    mediums = []
+    for tuple in list_tuples:
+        urls.append(tuple[0])
+        images.append(tuple[0])
+        sources.append(tuple[1])
+        mediums.append(tuple[2])
+    df['source'] = sources
+    df['web_url'] = urls
+    df['image_url'] = images
+    df['medium'] = mediums
     return df
 
 #INSTAGRAM
 
-insta_post = ['https://www.instagram.com/p/Br2LWE1hHOc/','https://www.instagram.com/p/Br2LHsDB4rL/',
-                 'https://www.instagram.com/p/Br2K-V8heFw/','https://www.instagram.com/p/Br1R6cjBI5j/',
-                 'https://www.instagram.com/p/Br1RjfphbWn/','https://www.instagram.com/p/Br1RYYthKBq/',
-                 'https://www.instagram.com/p/Bruqo-ihpaH/','https://www.instagram.com/p/BruqhH9BlJ0/',
-                 'https://www.instagram.com/p/Brrp-7PBqlf/','https://www.instagram.com/p/BrrpzXIBbgC/','https://www.instagram.com/p/Brmjo72ByfA/',
-                 'https://www.instagram.com/p/Brmjeg4huS_/','https://www.instagram.com/p/BrmjRICBXVk/','https://www.instagram.com/p/BrmhYKiBli5/']
+insta_post = [('https://www.instagram.com/p/Br2LWE1hHOc/','Daily Health Tips')
+                ,('https://www.instagram.com/p/Br2LHsDB4rL/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/Br2K-V8heFw/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/Br1R6cjBI5j/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/Br1RjfphbWn/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/Br1RYYthKBq/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/Bruqo-ihpaH/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/BruqhH9BlJ0/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/Brrp-7PBqlf/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/BrrpzXIBbgC/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/Brmjo72ByfA/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/Brmjeg4huS_/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/BrmjRICBXVk/','Daily Health Tips'),
+                 ('https://www.instagram.com/p/BrmhYKiBli5/','Daily Health Tips')]
 
-def create_sample_insta(list_urls, categories_list):
+def create_sample_insta(list_tuples, categories_list):
     diets = ['vegan','keto','vegetarian','paleo']
     df = pd.DataFrame()
-    df['web_url'] = list_urls
-    df['image_url'] = list_urls
+    # df['web_url'] = list_urls
+    # df['image_url'] = list_urls
     df['description'] = 'Instagram Dummy Data'
     df['title'] = 'Instagram Dummy Data'
     df['date'] = '2018-12-26'
-    df['source'] = 'Daily Health Tips'
+    # df['source'] = 'Daily Health Tips'
     df['source_id'] = 'Instagram'
     df['formality'] = 'Informal'
     df['medium'] = 'video'
@@ -136,4 +163,14 @@ def create_sample_insta(list_urls, categories_list):
     df['param_3'] = random_cat(categories_list,length)
     df['param_4'] = random_cat(categories_list,length)
     df['param_5'] = random_cat(categories_list,length)
+    urls = []
+    images = []
+    sources = []
+    for tuple in list_tuples:
+        urls.append(tuple[0])
+        images.append(tuple[0])
+        sources.append(tuple[1])
+    df['source'] = sources
+    df['web_url'] = urls
+    df['image_url'] = images
     return df
--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-from newspackage import *
-# from newspackage.etl import *
-
-
-# if __name__ == '__main__':
-#     app.run_server(debug = True)
